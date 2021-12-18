cb6d870b6ba704ffb1d6bf218d90b19e42e423f0
simyeen <simyeen@naver.com>
2021-12-18 16:40:49 +0900

    Feat: Team 관리 컴포넌트 폴더 따로 분할해서 관리

---
+++ src/components/Team/index.js
@@ -0,0 +1,50 @@
+import Component from "../../core/Component.js";
+import store, { setState } from "../../storage/Store.js";
+import { $ } from "../../common/dom.js";
+
+export default class Team extends Component {
+  mount() {
+    this.currentCourse = "frontend";
+    this.currentPlayGame = "baseball";
+    this.setEvent();
+  }
+
+  setEvent() {
+    $("#course-select").addEventListener("change", this.onChange.bind(this));
+    $("#mission-select").addEventListener("change", this.onChange.bind(this));
+    $("#show-team-matcher-button").addEventListener(
+      "click",
+      this.onClick.bind(this)
+    );
+  }
+
+  setState;
+
+  onChange({ target }) {
+    this.currentCourse = target.value;
+    console.log(this.currentCourse);
+  }
+
+  onClick(e) {
+    e.preventDefault();
+    console.log(this.currentCourse, this.currentPlayGame);
+  }
+
+  template() {
+    return `  
+      <section id="team-section">
+        <h3>팀 매칭을 관리할 코스, 미션을 선택하세요.</h3>
+        <form>
+          <select id="course-select">
+            <option value = "frontend">프론트엔드</option>
+            <option value = "backend" >백엔드</option>
+          </select>
+          <select id="mission-select">
+            <option value="baseball">숫자야구게임</option>
+          </select>
+          <button id="show-team-matcher-button">확인</button>
+        </form>
+      </section>
+    `;
+  }
+}
