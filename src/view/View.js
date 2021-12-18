f28037337fe273d1f256a7feb83272a618673b0c
minbr0ther <minbr0ther@naver.com>
2021-12-18 15:27:45 +0900

    feat: 케이스 별 매칭 컴포넌트를 숨긴다

--- src/view/View.js
+++ src/view/View.js
@@ -99,37 +99,39 @@ export default class View {
       </form>
     </section>
 
-    <section>
-      <h3>프론트엔드 숫자야구게임 미션의 팀 매칭</h3>
-      <div>
+    <div id="case-mached-component" >
+        <section id="not-mached-component" style="display: none;">
+        <h3>프론트엔드 숫자야구게임 미션의 팀 매칭</h3>
         <div>
-          <p>아직 매칭된 팀이 없습니다. 팀을 매칭하겠습니까?</p>
-          <form>
-            <label>1팀당 인원 수</label>
-            <input id="team-member-count-input" type="number" />
-            <button id="match-team-button">팀 매칭</button>
-          </form>
+            <div>
+            <p>아직 매칭된 팀이 없습니다. 팀을 매칭하겠습니까?</p>
+            <form>
+                <label>1팀당 인원 수</label>
+                <input id="team-member-count-input" type="number" />
+                <button id="match-team-button">팀 매칭</button>
+            </form>
+            </div>
+            <h4>크루 목록</h4>
+            <ul>
+            <li>준</li>
+            <li>포코</li>
+            </ul>
         </div>
-        <h4>크루 목록</h4>
-        <ul>
-          <li>준</li>
-          <li>포코</li>
-        </ul>
-      </div>
-    </section>
+        </section>
 
-    <!-- 팀 매칭이 된 경우 -->
-    <section>
-      <h3>프론트엔드 숫자야구게임 조회</h3>
-      <p>팀이 매칭되었습니다.</p>
-      <ul id="team-match-result">
-        <li>준,포코</li>
-      </ul>
-      <p>
-        팀을 재매칭 하시겠습니까?
-        <button id="rematch-team-button">재매칭</button>
-      </p>
-    </section>
+        <!-- 팀 매칭이 된 경우 -->
+        <section id="ok-mached-component"  style="display: none;">
+        <h3>프론트엔드 숫자야구게임 조회</h3>
+        <p>팀이 매칭되었습니다.</p>
+        <ul id="team-match-result">
+            <li>준,포코</li>
+        </ul>
+        <p>
+            팀을 재매칭 하시겠습니까?
+            <button id="rematch-team-button">재매칭</button>
+        </p>
+        </section>
+    </div>
   </main>
     `,
     );
