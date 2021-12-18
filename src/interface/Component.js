07c2fb084f60615777aa3098913f4a0b3cc3b9bc
NE5054 <w_lan123@naver.com>
2021-12-18 17:30:13 +0900

    feat: (Component) Component interface 구현

---
+++ src/interface/Component.js
@@ -0,0 +1,33 @@
+import { $ } from '../common/utils.js';
+import resources from '../constants/resources.js';
+import selectors from '../constants/selectors.js';
+
+export default class Component {
+    constructor(target, props) {
+        this.target = target;
+        this.props = props;
+
+        if (props?.key) {
+            this.key = props.key;
+            this.resources = resources[this.key];
+            this.selectors = selectors[this.key];
+        }
+
+        this.eventAttr = selectors.COMMON.EVENT_DATA_KEY;
+        this.setup();
+    }
+
+    setup() {}
+
+    mount() {}
+
+    willmount() {}
+
+    template() {}
+
+    render() {
+        this.willmount();
+        $(this.target).innerHTML = this.template();
+        this.mount();
+    }
+}
