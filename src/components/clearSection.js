02960d0a785727619779840307db0c47647dab0e
NaRae-tech <narae.dev@gmail.com>
2021-12-18 16:34:17 +0900

    동일한 html이 반복되지 않도록 변경(이전 section의 다른 버튼을 누르면 출력이 이상해지는 문제 미해결)

---
+++ src/components/clearSection.js
@@ -0,0 +1,8 @@
+import { $ } from '../dom.js'
+
+export default function ClearSection(selctionName) {
+  let container = $(`${selctionName}`);
+  while (container.hasChildNodes()) {
+    container.removeChild(container.lastChild);
+  }
+}
--- src/components/drawCrewTabContent.js
+++ src/components/drawCrewTabContent.js
@@ -1,10 +1,13 @@
 import { buttonIdElementCreate, h3ElementCreate, inputElementCreate, labelElemntCreate,theadElementCreate } from '../createElement.js';
 import { $ } from '../dom.js';
 import CrewNameValidity from './crewNameValidity.js';
-import CREW from '../index.js';
+import { frontEndCrew,backEndCrew } from '../index.js';
+import ClearSection from './clearSection.js';
 
 function drawCourseSection() {
+  ClearSection('main');
   const section = document.createElement('section');
+  section.id = 'crew-manage-choose-course';
   const sectionTitle = h3ElementCreate('크루를 관리할 코스를 선택해주세요.');
   const sectionDiv = document.createElement('div');
   const frontRadioInput = inputElementCreate({ "id": "frontend-course", "type": "radio" })
@@ -14,10 +17,13 @@ function drawCourseSection() {
   const backRadioInput = inputElementCreate({ "id": "backend-course", "type": "radio" });
   backRadioInput.name = "course";
   backRadioInput.value = "backend";
-  const backRadioLabel = labelElemntCreate({ "for": "backend","text":"백엔드" });
+  const backRadioLabel = labelElemntCreate({ "for": "backend", "text": "백엔드" });
   sectionDiv.append(frontRadioInput, frontRadioLabel, backRadioInput, backRadioLabel);
   section.append(sectionTitle, sectionDiv);
   $('main').appendChild(section);
+  const nextSection = document.createElement('section');
+  nextSection.id = 'add-crew-member';
+  $('main').appendChild(nextSection);
 }
 function chooseCourse() {
   const courses = document.getElementsByName("course");
@@ -27,8 +33,9 @@ function chooseCourse() {
   }
 }
 function drawCrewManageSelction(e) {
+  ClearSection('#add-crew-member');
+  const section = $('#add-crew-member');
   let course = (e.target.value==="frontend")?"프론트엔드":"백엔드";
-  const section = document.createElement('section');
   const sectionTitle = h3ElementCreate(`${course} 크루 관리`);
   const sectionForm = document.createElement('form');
   const label = labelElemntCreate({ "text": "크루이름" });
@@ -37,29 +44,42 @@ function drawCrewManageSelction(e) {
   sectionForm.append(label,crewNameInput,crewNameButton);
   section.append(sectionTitle, sectionForm);
   $('main').appendChild(section);
+  const nextSection = document.createElement('section');
+  nextSection.id = 'crew-member-table';
+  $('main').appendChild(nextSection);
   $('#add-crew-button').addEventListener('click', function () { drawCrewTableSelection(course); });
+}
+function addCrewNameToList(mode, name) {
+  let crew = (mode === "프론트엔드") ? frontEndCrew : backEndCrew;
+  let course = (mode === "프론트엔드") ? 'frontend' : 'backend';
+  crew.push(name);
+  localStorage.setItem(course, JSON.stringify(crew));
 }
 function drawCrewTableSelection(course) {
   const name = $('#crew-name-input').value;
-  if (CrewNameValidity(name)) {
-    const section = document.createElement('section');
+  if (CrewNameValidity(course, name)) {
+    addCrewNameToList(course, name);
+    ClearSection('#crew-member-table');
+    const section = $('#crew-member-table');
     const sectionTitle = h3ElementCreate(`${course} 크루 목록`);
-    const table = crewTableBasic();
+    const table = crewTableBasic(course);
     section.append(sectionTitle, table);
     $('main').appendChild(section);
   }
 }
-function crewTableBasic() {
+function crewTableBasic(course) {
   const table = document.createElement('table');
   const thead = theadElementCreate(['', '크루', '관리']);
   const tbody = document.createElement('tbody');
-  for (let i = 0; i < CREW.length; i++){
-    tbody.appendChild(crewTableContent(CREW[i]));
+  const crew = (course === "프론트엔드") ? frontEndCrew : backEndCrew;
+  for (let i = 0; i < crew.length; i++){
+    tbody.appendChild(crewTableContent(i, crew[i]));
   }
   table.append(thead, tbody);
+  activateDeleteCrewButton();
   return table;
 }
-function crewTableContent(name) {
+function crewTableContent(i, name) {
   const tr = document.createElement('tr');
   const td = document.createElement('td');
   const countTd = td.cloneNode(true);
@@ -70,6 +90,15 @@ function crewTableContent(name) {
   buttonTd.innerHTML = `<button class="delete-crew-button" type="button">삭제</button>`;
   tr.append(countTd, nameTd, buttonTd);
   return tr;
+}
+function activateDeleteCrewButton() {
+  let deleteBtns = document.querySelectorAll('.delete-crew-button');
+  for (let i = 0; i < deleteBtns.length; i++){
+    deleteBtns[i].addEventListener('click', deleteCrew);
+  }
+}
+function deleteCrew(e) {
+  console.log(e.path)
 }
 export default function DrawCrewTabContent() {
   drawCourseSection();
