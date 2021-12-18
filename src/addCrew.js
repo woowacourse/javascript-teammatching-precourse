9827192536bab9f63a126a0cc058b9276f169b38
skdms3030 <88518471+skdms3030@users.noreply.github.com>
2021-12-18 17:06:42 +0900

    Feat: 크루 이름 예외처리 추가

--- src/addCrew.js
+++ src/addCrew.js
@@ -1,23 +1,43 @@
-import { CREW, FRONTCREW, BACKCREW } from "./constant.js"
+import { CREW, FRONTCREW, BACKCREW, ERROR } from "./constant.js"
 import { printCrewTable } from './printCrewTable.js';
 
+function chceckCrewNameValid($crewNameInputValue){
+    if($crewNameInputValue.trim() === ''){
+        alert(ERROR.CREW_NAME_EMPTY_ERROR)
+        return false
+    }
+    else if($crewNameInputValue.length > 5){
+        alert(ERROR.CREW_NAME_LENGTH_ERROR)
+        return false
+    }
+    else if(CREW.COURSE === 'Frontend' && CREW.FRONTCREWLIST.length >0 && CREW.FRONTCREWLIST.indexOf($crewNameInputValue) === -1){
+            alert(ERROR.CREW_NAME_DUPLICATE_ERROR)
+            return false
+        }            
+    else if(CREW.COURSE === 'Backend' && CREW.BACKCREWLIST.length >0 && CREW.BACKCREWLIST.indexOf($crewNameInputValue) === -1){
+            alert(ERROR.CREW_NAME_DUPLICATE_ERROR)
+            return false
+        }     
+
+    return true
+}
 
 export function addCrew(){
     console.log(CREW.COURSE)
     let $crewNameInputValue = document.querySelector('#crew-name-input').value
 
-
-    if(CREW.COURSE === 'Frontend'){
-        $crewNameInputValue = new FRONTCREW($crewNameInputValue)
-        CREW.FRONTCREWLIST.push($crewNameInputValue)
-        console.log(CREW.FRONTCREWLIST)   
+    if(chceckCrewNameValid($crewNameInputValue)){
+        if(CREW.COURSE === 'Frontend'){
+            $crewNameInputValue = new FRONTCREW($crewNameInputValue)
+            CREW.FRONTCREWLIST.push($crewNameInputValue)
+            console.log(CREW.FRONTCREWLIST)   
+        }
+        else if(CREW.COURSE === 'Backend'){
+            $crewNameInputValue = new BACKCREW($crewNameInputValue)
+            CREW.BACKCREWLIST.push($crewNameInputValue)
+            console.log(CREW.BACKCREWLIST)  
+        }        
     }
-    else if(CREW.COURSE === 'Backend'){
-        $crewNameInputValue = new BACKCREW($crewNameInputValue)
-        CREW.BACKCREWLIST.push($crewNameInputValue)
-        console.log(CREW.BACKCREWLIST)  
-    }
-
 
     printCrewTable();
   
--- src/constant.js
+++ src/constant.js
@@ -4,6 +4,12 @@ export const CREW = {
     BACKCREWLIST : []
 }
 
+export const ERROR = {
+    CREW_NAME_EMPTY_ERROR : '크루이름은 공백일 수 없습니다.',
+    CREW_NAME_LENGTH_ERROR : '크루이름은 5글자를 초과할 수 없습니다.',
+    CREW_NAME_DUPLICATE_ERROR : '크루이름은 중복될 수 없습니다.'
+}
+
 export class FRONTCREW{
     constructor(name){
         this.name = name;
