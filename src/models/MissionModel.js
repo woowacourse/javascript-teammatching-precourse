cd8c3d56bf2ced9076fdfc6ba19775b77a31ddc4
최광희 <polyckh@gmail.com>
2021-12-18 15:22:03 +0900

    feat: 미션 모델을 위한 MissionModel.js 생성

---
+++ src/models/MissionModel.js
@@ -0,0 +1,15 @@
+export default {
+  missions: [
+    '숫자야구게임',
+    '자동차경주',
+    '로또',
+    '장바구니',
+    '결제',
+    '지하철노선도',
+    '성능개선',
+    '배포',
+  ],
+  list() {
+    return this.missions;
+  },
+};
