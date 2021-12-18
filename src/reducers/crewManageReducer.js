4ec6a2f342c373326503fcd9145e4830e0d04a6a
airman5573 <rpf5573@kakao.com>
2021-12-18 17:24:53 +0900

    fix(crewManageReducer): add actions for adding crew

--- src/reducers/crewManageReducer.js
+++ src/reducers/crewManageReducer.js
@@ -1,10 +1,15 @@
 import { ELEMENT_SELECTOR } from '../constants/index.js';
 import createAction from '../flux/actionCreator.js';
 
-export const UPDATE_ACTIVE_COURSE = 'team-matching/crew-manage/UPDATE_ACTIVE_COURSE';
+export const UPDATE_ACTIVE_COURSE = 'team-matching-board/crew-manage/UPDATE_ACTIVE_COURSE';
+export const ADD_CREW = 'team-matching-board/crew-manage/ADD_CREW';
 
 export const updateActiveCourseAction = (courseId) => {
   return createAction(UPDATE_ACTIVE_COURSE, courseId);
+};
+
+export const addCrewAction = (crew) => {
+  return createAction(ADD_CREW, crew);
 };
 
 const initialState = {
@@ -17,6 +22,11 @@ const crewManageReducer = (state = initialState, { type, data }) => {
       return {
         ...state,
         activeCourseId: data,
+      };
+    case ADD_CREW:
+      return {
+        ...state,
+        crews: [...state.crews, data],
       };
     default:
       return { ...state };
