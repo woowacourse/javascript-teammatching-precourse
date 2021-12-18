e20123892efafb8f422d8d9a3b7425af3cd79245
leegwae <leegwae@gmail.com>
2021-12-18 15:53:31 +0900

    feat: 선택한 코스에 따라 헤딩 바꾸기

---
+++ src/components/crewPanel/const.js
@@ -0,0 +1,2 @@
+export const ID_INPUT_NAME = 'crew-name-input';
+export const ID_BUTTON_ADD = 'add-crew-buttton';
--- src/components/crewPanel/index.js
+++ src/components/crewPanel/index.js
@@ -1,6 +1,9 @@
 import Panel from '../common/Panel.js';
 import setVisibility from '../../dom/utils/setVisibility.js';
 import getChildrenToList from '../../dom/utils/getChildrenToList.js';
+import { ID_BUTTON_ADD, ID_INPUT_NAME } from './const.js';
+import removeChildren from '../../dom/utils/removeChildren.js';
+import { DICT_COURSE } from '../../../data/course.js';
 
 export default class CrewPanel extends Panel {
 	constructor(panel) {
@@ -10,6 +13,8 @@ export default class CrewPanel extends Panel {
 
 		sections.forEach((section, i) => setVisibility(section, i === 0));
 		this.initCourseSection();
+		this.initCrewSection();
+		this.initTableSection();
 	}
 
 	initCourseSection() {
@@ -22,7 +27,36 @@ export default class CrewPanel extends Panel {
 		});
 	}
 
+	initCrewSection() {
+		const form = this.crewSection.getElementsByTagName('form').item(0);
+		const input = form.getElementsByTagName('input').item(0);
+		input.id = ID_INPUT_NAME;
+		const button = form.getElementsByTagName('button').item(0);
+		button.id = ID_BUTTON_ADD;
+	}
+
+	initTableSection() {
+		const table = this.tableSection.getElementsByTagName('table').item(0);
+		const tbody = table.getElementsByTagName('tbody').item(0);
+		removeChildren(tbody);
+	}
+
+	setCrewSection(course) {
+		const heading = this.crewSection.getElementsByTagName('h3').item(0);
+		heading.innerText = `${DICT_COURSE[course]} 크루 관리`;
+	}
+
+	setTableSection(course) {
+		const heading = this.tableSection.getElementsByTagName('h3').item(0);
+		heading.innerText = `${DICT_COURSE[course]} 크루 목록`;
+	}
+
 	onRadioButtonClick(e) {
-		console.log(e.target.value);
+		const course = e.target.value;
+
+		setVisibility(this.crewSection, true);
+		this.setCrewSection(course);
+		setVisibility(this.tableSection, true);
+		this.setTableSection(course);
 	}
 }
