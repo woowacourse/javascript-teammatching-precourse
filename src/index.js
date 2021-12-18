eb3e942cb8fa99300acc96d2969e907e15f58283
nan-noo <uni613@naver.com>
2021-12-18 14:40:20 +0900

    feat: get crew input & check input
    Add functions
    - onCourseRadioClick
    - onAddCrewClick
    - checkCrewInput
    - isSameName

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 import { fetchHtmlView } from './fetch.js';
 
+
+let crewList = [{name: '준', course: '프론트엔드'}];
+let selectedCourse = 'frontend';
+
+function onCourseRadioClick(e) {
+    selectedCourse = e.target.value;
+}
+
+function onAddCrewClick() {
+    const newCrewName = document.querySelector("#crew-name-input").value;
+    if(checkCrewInput(newCrewName)) console.log('hihi');
+
+}
+
+function checkCrewInput(name) {
+    return !isSameName(name.trim()) && name.trim().length <= 5 && name.trim().length >= 1; 
+}
+
+function isSameName(name) {
+    return crewList.filter(crew => crew.name === name).length;
+}
+
+//
 function renderView(fileName) {
     fetchHtmlView(fileName).then(view => app.innerHTML = view);
 }
@@ -16,8 +39,11 @@ function eventHandler(e) {
     e.preventDefault();
 
     const handlers = {
-        "crew-tab"() {onCrewTabClick();},
-        "team-tab"() {onTeamTabClick();},
+        "crew-tab"() { onCrewTabClick(); },
+        "team-tab"() { onTeamTabClick(); },
+        "frontend-course"() { onCourseRadioClick(e) },
+        "backend-course"() { onCourseRadioClick(e) },
+        "add-crew-buttton"() { onAddCrewClick(); }
     };
     if(Object.keys(handlers).includes(e.target.id)) handlers[e.target.id]();
 }
