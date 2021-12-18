6868105cc93276968265768cc882a8fa4b037eaa
NE5054 <w_lan123@naver.com>
2021-12-18 17:21:12 +0900

    feat: dom 선택자, uid 생성, 랜덤 데이터 생성 기능 구현

---
+++ src/common/utils.js
@@ -0,0 +1,31 @@
+const { shuffle } = MissionUtils.Random;
+
+export const $ = (selector, target = document) => target.querySelector(selector);
+export const $all = (selector, target = document) => new Array(...target.querySelectorAll(selector));
+
+export const getUID = (function () {
+    let id = 0;
+    return function () {
+        return id++;
+    };
+})();
+
+export const shuffleArray = (arr) => {
+    const sequence = shuffle(new Array(arr.length).fill(0).map((item, idx) => idx));
+
+    return arr.map((item, idx) => arr[sequence[idx]]);
+};
+
+export const matchTeam = (_members, number) => {
+    const members = shuffleArray(_members);
+    const teams = [];
+
+    while (members.length >= number) {
+        teams.push(members.splice(0, number));
+        console.log(teams);
+    }
+
+    members.forEach((member, idx) => teams[idx % teams.length].push(member));
+
+    return teams;
+};
