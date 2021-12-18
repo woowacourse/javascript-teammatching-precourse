4ff4ffda787a9e2f160a9458edaf0c68963e036a
sanaandmomo <qajjang46@naver.com>
2021-12-18 14:44:50 +0900

    fix: 크루 이름 숫자 체크 로직에서 오직 문자열인지 판별하는 로직으로 변경해라

--- src/validation/index.js
+++ src/validation/index.js
@@ -18,11 +18,11 @@ const isLessThanFive = isLess(5);
 
 const isDense = (value) => value.replace(/\s/g, '').length === value.length;
 
-const hasNum = (value) => value.match(/\d+/g) !== null;
+const isOnlyString = (value) => value.match(/\d+/g) === null;
 
 export const checkCrewName = createCheckEveryFunction([
     createValidFunction(isRequired, '크루 이름을 입력해주세요.'),
     createValidFunction(isLessThanFive, '5자 이하로 설정해주세요.'),
     createValidFunction(isDense, '공백이 있으면 안됩니다.'),
-    createValidFunction(hasNum, '숫자가 있으면 안됩니다.'),
+    createValidFunction(isOnlyString, '숫자가 있으면 안됩니다.'),
 ]);
