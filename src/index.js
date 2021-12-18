4c5ecf268d92a0037554e7a41c48df0c8324cc65
NaRae-tech <narae.dev@gmail.com>
2021-12-18 16:01:13 +0900

    front의 crewlist와 back의 crewlist분리

--- src/index.js
+++ src/index.js
@@ -2,9 +2,10 @@ import { $ } from './dom.js';
 import { HEADER, MAIN } from './markup.js';
 import DrawCrewTabContent from './components/drawCrewTabContent.js';
 import DrawTeamTabContent from './components/drawTeamTabContent.js';
-import Crew from './components/crew.js';
 
-export let CREW = new Crew();
+let frontEndCrew = [];
+let backEndCrew = [];
+export default {frontEndCrew,backEndCrew};
 
 export class Board {
   drawMainTitle(){
@@ -15,12 +16,24 @@ export class Board {
     $('#crew-tab').addEventListener('click',DrawCrewTabContent);
     $('#team-tab').addEventListener('click',DrawTeamTabContent);
   }
+  crewListCheck() {
+    if (localStorage.getItem('frontend')) {
+      frontEndCrew = JSON.parse(localStorage.getItem('frontend'));
+    }
+    else {
+      localStorage.setItem('frontend', JSON.stringify(frontEndCrew));
+    }
+    if (localStorage.getItem('backend')) {
+      backEndCrew = JSON.parse(localStorage.getItem('backend'));
+    }
+    else {
+      localStorage.setItem('backend', JSON.stringify(backEndCrew));
+    }
+  }
   start() {
     this.drawMainTitle();
     this.tabButtonsEventListener();
-    if (localStorage.getItem('crewList')) {
-      CREW = JSON.parse(localStorage.getItem('crewList'));
-    }
+    this.crewListCheck();
   }
 }
 const board = new Board();
