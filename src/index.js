977b6cc7c6af6cf30603d6c47239e091058f432a
nan-noo <uni613@naver.com>
2021-12-18 14:58:00 +0900

    feat: delete crew click delete button
    Add functions
    - onDeleteCrewClick
    - renderDeleteRow

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import { fetchHtmlView } from './fetch.js';
 import { INCORRECT_CREW_NAME } from './constants/alert-messages.js';
 
-
 let crewList = [{name: '준', course: '프론트엔드'}];
 let selectedCourse = 'frontend';
 
@@ -21,6 +20,16 @@ function checkCrewInput(name) {
 
 function isSameName(name) {
     return crewList.filter(crew => crew.name === name).length;
+}
+
+function onDeleteCrewClick(target) {
+    const row = target.parentElement.parentElement;
+    renderDeleteRow(row.rowIndex);
+}
+
+function renderDeleteRow(rowIndex) {
+    const crewTable = document.querySelector("#crew-table");
+    crewTable.deleteRow(rowIndex);
 }
 
 //
@@ -44,9 +53,12 @@ function eventHandler(e) {
         "team-tab"() { onTeamTabClick(); },
         "frontend-course"() { onCourseRadioClick(e) },
         "backend-course"() { onCourseRadioClick(e) },
-        "add-crew-buttton"() { onAddCrewClick(); }
+        "add-crew-buttton"() { onAddCrewClick(); },
+        "delete-crew-buttton"() { onDeleteCrewClick(e.target); },
     };
-    if(Object.keys(handlers).includes(e.target.id)) handlers[e.target.id]();
+    const handlerKeys = Object.keys(handlers);
+    if(handlerKeys.includes(e.target.id)) handlers[e.target.id]();
+    else if(handlerKeys.includes(e.target.className)) handlers[e.target.className]();
 }
 
 const app = document.querySelector("#app");
