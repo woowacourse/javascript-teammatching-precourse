f0fc35e9531786959d1f08f32241d1c022352d3e
SoYi Jeon <soyijeon47@gmail.com>
2021-12-18 16:47:57 +0900

    feat: 팀 매칭 관리 - 재매칭 버튼 기능

--- src/controllers/TeamTab.js
+++ src/controllers/TeamTab.js
@@ -22,13 +22,18 @@ export default class TeamTab {
     matchTeamButton.addEventListener('click', this.onClickMatchTeam.bind(this));
   }
 
+  setRematchTeamEvent() {
+    const rematchTeamButton = document.querySelector('#rematch-team-button');
+    rematchTeamButton.addEventListener('click', this.onClickRematchTeam.bind(this));
+  }
+
   onClickShowTeamMatcher(e) {
     e.preventDefault();
     const courseSelect = document.querySelector('#course-select');
     const missionSelect = document.querySelector('#mission-select');
     this.course = courseSelect.value;
     this.mission = missionSelect.value;
-    this.view.renderMatcher(this.course, this.mission, this.storage.crew[courseSelect.value]);
+    this.view.renderMatcher(this.course, this.mission, this.storage.crew[this.course]);
     this.setMatchTeamEvent();
   }
 
@@ -38,5 +43,11 @@ export default class TeamTab {
     const memberCount = Number(memberCountInput.value);
     const matchResult = matchTeam(this.storage.crew[this.course], memberCount);
     this.view.renderMatchResult(this.course, this.mission, matchResult);
+    this.setRematchTeamEvent();
+  }
+
+  onClickRematchTeam() {
+    this.view.renderMatcher(this.course, this.mission, this.storage.crew[this.course]);
+    this.setMatchTeamEvent();
   }
 }
