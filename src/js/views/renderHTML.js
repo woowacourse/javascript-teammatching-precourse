04b1507e72f764cb44b7c70a44dbbbf484d90b07
KIM YEJI <rladpwl0512@gmail.com>
2021-12-18 15:36:08 +0900

    fix: 숫자야구 게임 옵션에 value 추가

--- src/js/views/renderHTML.js
+++ src/js/views/renderHTML.js
@@ -71,7 +71,7 @@ export const renderTeamManager = () => {
         <option value="backend">백엔드</option>
       </select>
       <select id="mission-select">
-        <option>숫자야구게임</option>
+        <option value="baseball">숫자야구게임</option>
         <option value="racingcar">자동차경주</option>
         <option value="lotto">로또</option>
         <option value="shopping-cart">장바구니</option>
@@ -83,35 +83,7 @@ export const renderTeamManager = () => {
       <button id="show-team-matcher-button">확인</button>
     </form>
   </section>
-  <section>
-    <h3>프론트엔드 숫자야구게임 미션의 팀 매칭</h3>
-    <div>
-      <div>
-        <p>아직 매칭된 팀이 없습니다. 팀을 매칭하겠습니까?</p>
-        <form>
-          <label>1팀당 인원 수</label>
-          <input id="team-member-count-input" type="number" />
-          <button id="match-team-button">팀 매칭</button>
-        </form>
-      </div>
-      <h4>크루 목록</h4>
-      <ul id="team-match-result">
-        <li>준</li>
-        <li>포코</li>
-      </ul>
-    </div>
-  </section>
-  <!-- 팀 매칭이 된 경우 -->
-  <section>
-    <h3>프론트엔드 숫자야구게임 조회</h3>
-    <p>팀이 매칭되었습니다.</p>
-    <ul>
-      <li>준,포코</li>
-    </ul>
-    <p>
-      팀을 재매칭 하시겠습니까?
-      <button id="rematch-team-button">재매칭</button>
-    </p>
-  </section>
+  
+  
   `;
 };
