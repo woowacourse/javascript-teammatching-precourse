0e7f8448c76c6d4e43e7aa5ee83f4e98d89e6df9
최광희 <polyckh@gmail.com>
2021-12-18 13:45:44 +0900

    feat: 루 관리 탭 클릭시 크루 관리 뷰를 보여준다.

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -1,5 +1,6 @@
 import { $ } from '../utils/dom.js';
 
+import CrewManageView from '../views/CrewManageView.js';
 import LayoutView from '../views/LayoutView.js';
 import TabView from '../views/TabView.js';
 
@@ -7,11 +8,24 @@ export default {
   init() {
     LayoutView.setup($('#app'));
     TabView.setup($('#tab-view')).on('@changeTab', (e) => this.onChangeTabView(e.detail.tab));
+    CrewManageView.setup($('main'));
 
     this.selectedTab = '크루 관리';
+    this.renderView();
+  },
+
+  renderView() {
+    switch (this.selectedTab) {
+      case '크루 관리':
+        CrewManageView.render();
+        break;
+      default:
+        break;
+    }
   },
 
   onChangeTabView(tab) {
     this.selectedTab = tab;
+    this.renderView();
   },
 };
