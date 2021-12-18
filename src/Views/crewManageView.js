3b7d5e7d5df5196661f47b244cfc9e4dd31d3bfa
kamwoo <rkadndud06@naver.com>
2021-12-18 13:59:19 +0900

    feat: crew manage view 작성
    - crew manage 페이지의 ui 로직을 담당하는 클래스 작성

---
+++ src/Views/crewManageView.js
@@ -0,0 +1,8 @@
+import { crewManageTemplate } from '../Templates/crewManageTemplate.js';
+import { $ } from '../Utils/common.js';
+
+export default class CrewManageView {
+  render() {
+    $(`main`).innerHTML = crewManageTemplate;
+  }
+}
