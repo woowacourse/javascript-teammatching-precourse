84460e57c7dfea211fe668447a7886446466ac42
leegwae <leegwae@gmail.com>
2021-12-18 13:32:40 +0900

    feat: 객체로 데이터 불러올 수 있도록 함

---
+++ data/course.js
@@ -0,0 +1,5 @@
+export const COURSE = ['frontend', 'backend'];
+export const DICT_COURSE = {
+	frontend: '프론트엔드',
+	backend: '백엔드',
+};
---
+++ data/mission.js
@@ -0,0 +1,20 @@
+export const MISSION = [
+	'baseball',
+	'racingcar',
+	'lotto',
+	'shopping-cart',
+	'payments',
+	'subway',
+	'performance',
+	'deploy',
+];
+export const DICT_MISSION = {
+	baseball: '숫자야구게임',
+	racingcar: '자동차경주',
+	lotto: '로또',
+	'shopping-cart': '장바구니',
+	payments: '결제',
+	subway: '지하철노선도',
+	performance: '성능개선',
+	deploy: '배포',
+};
