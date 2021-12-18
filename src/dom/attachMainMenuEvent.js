b7aab12fb11412414f39d1a1603a5863ad9b6ebc
Chamy619 <yd95069506@naver.com>
2021-12-18 15:06:04 +0900

    fix: 크루 관리 메뉴 여러번 클릭시 코스 선택하는 라디오 버튼이 여러개 출력되는 문제 해결

--- src/dom/attachMainMenuEvent.js
+++ src/dom/attachMainMenuEvent.js
@@ -1,9 +1,15 @@
-import { MAIN_MENU } from '../constants.js';
+import { MAIN, MAIN_MENU } from '../constants.js';
 import printCourseSelectionSection from './printCourseSelectionSection.js';
+
+const clearMain = () => {
+  const $main = document.querySelector(MAIN);
+  $main.innerHTML = '';
+};
 
 const attachMainMenuEvent = () => {
   const $crewTab = document.getElementById(MAIN_MENU[0].id);
   $crewTab.addEventListener('click', () => {
+    clearMain();
     printCourseSelectionSection();
   });
 };
