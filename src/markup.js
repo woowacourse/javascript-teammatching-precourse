65c030b1995833d1cd02dabf607dcd1417d397e5
NaRae-tech <narae.dev@gmail.com>
2021-12-18 16:41:44 +0900

    불필요한 상수변수 삭제

--- src/markup.js
+++ src/markup.js
@@ -1,5 +1,5 @@
-export const HEADER = 
-`<header>
+export const HEADER =
+  `<header>
     <h1>우테코 크루와 팀 매칭 관리 보드</h1>
     <nav>
       <ul>
@@ -11,51 +11,9 @@ export const HEADER =
         </li>
       </ul>
     </nav>
-  </header>`
-export const MAIN = 
-  `<main></main>`
+  </header>`;
+export const MAIN = '<main></main>';
 
-export const CREW_MANAGE_MAIN =
-`<main>
-    <section>
-      <h3>크루를 관리할 코스를 선택해주세요</h3>
-      <div>
-        <input id="frontend-course" type="radio" name="course" value="frontend" />
-        <label for="frontend">프론트엔드</label>
-        <input id="backend-course" type="radio" name="course" value="backend" />
-        <label for="backend">백엔드</label>
-      </div>
-    </section>
-    <section>
-      <h3>프론트엔드 크루 관리</h3>
-      <form>
-        <label>크루 이름</label>
-        <input id="crew-name-input" type="text" />
-        <button id="add-crew-button">확인</button>
-      </form>
-    </section>
-    <section>
-      <h3>프론트엔드 크루 목록</h3>
-      <table class="crew-table" border="1">
-        <thead>
-          <tr>
-            <th></th>
-            <th>크루</th>
-            <th>관리</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td>1</td>
-            <td>준</td>
-            <td>
-              <button class="delete-crew-button">삭제</button>
-            </td>
-          </tr>
-        </tbody>
-      </table>
-    </section>
-  </main>`
 export const TEAM_MATCHING_MAIN =
 ` <main>
     <section>
