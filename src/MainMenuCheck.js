40238bee65a5fe82cc835c84137e594211e3a41c
kevin7498 <kevin7498@naver.com>
2021-12-18 13:33:36 +0900

    feat: 어떤 메뉴를 눌렀는지 체크하는 클래스 'MainMenuCheck' 구현

---
+++ src/MainMenuCheck.js
@@ -0,0 +1,13 @@
+export default class MainMenuCheck {
+    static checkFirstMenu(targetId) {
+        if(targetId === 'crew-tab') {
+            ;
+        }
+    }
+
+    static checkSecondMenu(targetId) {
+        if(targetId === 'team-tab') {
+            ;
+        }
+    }
+}
