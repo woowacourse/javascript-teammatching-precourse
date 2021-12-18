a1e47e4e20a0f2162f090be277e31c8f37846b84
leegwae <leegwae@gmail.com>
2021-12-18 13:25:42 +0900

    chore: set up eslint with prettier

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+	env: {
+		browser: true,
+		es2021: true,
+		node: true,
+	},
+	extends: ['airbnb-base', 'plugin:prettier/recommended'],
+	parserOptions: {
+		ecmaVersion: 12,
+		sourceType: 'module',
+	},
+	plugins: ['import'],
+	rules: {
+		'no-console': 'off',
+		'no-alert': 'off',
+		'no-new': 'off',
+		'no-plusplus': 'off',
+		'import/extensions': 'off',
+		'import/prefer-default-export': 'off',
+		'class-methods-use-this': 'off',
+	},
+};
