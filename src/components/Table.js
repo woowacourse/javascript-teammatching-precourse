c808133f232eacdee4d48ebedac8346c34ac8cfd
NE5054 <w_lan123@naver.com>
2021-12-18 17:32:21 +0900

    feat: (Table) Table Component 구현

---
+++ src/components/Table.js
@@ -0,0 +1,45 @@
+import Component from '../interface/component.js';
+
+export default class Table extends Component {
+    setConfig(configuration) {
+        this.columns = configuration.columns ?? [];
+        this.data = configuration.data ?? [];
+        this.id = configuration.id ?? '';
+    }
+
+    itemTemplate(data, idx) {
+        return this.columns
+            .map((col) => {
+                let innerContent;
+
+                if (col.render !== undefined) innerContent = col.render(data, idx);
+                else innerContent = data[col.key];
+
+                return `<td>${innerContent}</td>`;
+            })
+            .join('');
+    }
+
+    itemListTemplate() {
+        return this.data
+            .map(
+                (data, idx) => `
+                <tr>${this.itemTemplate(data, idx)}</tr>
+            `
+            )
+            .join('');
+    }
+
+    columnTemplate() {
+        return this.columns.map((col) => `<th>${col.label}</th>`).join('');
+    }
+
+    template() {
+        return `
+            <table border="1" id="${this.id}">
+                <thead><tr>${this.columnTemplate()}</tr></thead>
+                <tbody>${this.itemListTemplate()}</tbody>
+            </table>
+        `;
+    }
+}
