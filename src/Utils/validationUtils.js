9e1afc6342074c1196aca51721d4c3e7a8a58933
kamwoo <rkadndud06@naver.com>
2021-12-18 15:29:05 +0900

    feat: validatoin utils 작성
    - 이름을 입력받을 때 유효성을 검사하는 메소드 추가

---
+++ src/Utils/validationUtils.js
@@ -0,0 +1,7 @@
+export default class ValidationUtils {
+  static checkInputName(text, list) {
+    if (!text) throw new Error('이름을 입력해주세요.');
+    if (text.length > 5) throw new Error('이름을 5글자 이하로 입력해주세요');
+    if (list.includes(text)) throw new Error('동일한 크루 이름이 존재합니다.');
+  }
+}
