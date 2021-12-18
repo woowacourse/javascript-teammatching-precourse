138fa422b1c3f9e3725f04309a14c94f267598af
NE5054 <w_lan123@naver.com>
2021-12-18 17:22:20 +0900

    feat: localStorage를 통해 통합적인 데이터 관리를 지원하는 모듈 구현

---
+++ src/store/index.js
@@ -0,0 +1,58 @@
+class Store {
+    constructor() {
+        this.storage = localStorage.getItem('state');
+        this.callback = () => {};
+
+        if (this.storage !== null) {
+            this.state = JSON.parse(this.storage);
+        } else this.state = {};
+    }
+
+    createStore(initialization) {
+        if (this.storage !== null) return;
+
+        this.state = { ...initialization };
+        this.apply();
+    }
+
+    setState(key, value) {
+        this.state = {
+            ...this.state,
+            [key]: value,
+        };
+
+        this.apply();
+    }
+
+    setStates(states) {
+        this.state = {
+            ...this.state,
+            ...states,
+        };
+
+        this.apply();
+    }
+
+    getState(selector) {
+        return selector(this.state);
+    }
+
+    apply() {
+        localStorage.setItem('state', JSON.stringify(this.state));
+        this.callback();
+    }
+
+    subscribe(callback) {
+        this.callback = callback;
+    }
+}
+
+const store = new Store();
+
+export const createStore = store.createStore.bind(store);
+export const subscribe = store.subscribe.bind(store);
+export const setStates = store.setStates.bind(store);
+export const setState = store.setState.bind(store);
+export const getState = store.getState.bind(store);
+
+export default store;
