5c6f4cb089cf22595525b848b3051be7334cbdfb
Ahn <jin7969@gmail.com>
2021-12-18 14:09:04 +0900

    feat: 각 해당 요소에 id값 입력

--- src/utils/template.js
+++ src/utils/template.js
@@ -4,10 +4,10 @@ export const mainTemplate = `
     <nav>
       <ul>
         <li>
-          <button>크루 관리</button>
+          <button id="crew-tab">크루 관리</button>
         </li>
         <li>
-          <button>팀 매칭 관리</button>
+          <button id="team-tab">팀 매칭 관리</button>
         </li>
       </ul>
     </nav>
@@ -20,9 +20,9 @@ export const crewManageTemplate = `
     <section>
       <h3>크루를 관리할 코스를 선택해주세요</h3>
       <div>
-        <input type="radio" name="course" value="frontend" />
+        <input id="frontend-course" type="radio" name="course" value="frontend" />
         <label for="frontend">프론트엔드</label>
-        <input type="radio" name="course" value="backend" />
+        <input id="backend-course" type="radio" name="course" value="backend" />
         <label for="backend">백엔드</label>
       </div>
     </section>
@@ -30,13 +30,13 @@ export const crewManageTemplate = `
       <h3>프론트엔드 크루 관리</h3>
       <form>
         <label>크루 이름</label>
-        <input type="text" />
-        <button>확인</button>
+        <input id="crew-name-input" type="text" />
+        <button id="add-crew-buttton">확인</button>
       </form>
     </section>
     <section>
       <h3>프론트엔드 크루 목록</h3>
-      <table border="1">
+      <table id="crew-table" border="1">
         <thead>
           <tr>
             <th></th>
@@ -49,7 +49,7 @@ export const crewManageTemplate = `
             <td>1</td>
             <td>준</td>
             <td>
-              <button>삭제</button>
+              <button id="delete-crew-buttton">삭제</button>
             </td>
           </tr>
         </tbody>
@@ -63,14 +63,14 @@ export const teamManageTemplate = `
     <section>
       <h3>팀 매칭을 관리할 코스, 미션을 선택하세요.</h3>
       <form>
-        <select>
+        <select id="course-select">
           <option>프론트엔드</option>
           <option>백엔드</option>
         </select>
-        <select>
+        <select id="mission-select">
           <option>숫자야구게임</option>
         </select>
-        <button>확인</button>
+        <button id="show-team-matcher-button">확인</button>
       </form>
     </section>
     <section>
@@ -80,8 +80,8 @@ export const teamManageTemplate = `
           <p>아직 매칭된 팀이 없습니다. 팀을 매칭하겠습니까?</p>
           <form>
             <label>1팀당 인원 수</label>
-            <input type="number" />
-            <button>팀 매칭</button>
+            <input id="team-member-count-input" type="number" />
+            <button id="match-team-button">팀 매칭</button>
           </form>
         </div>
         <h4>크루 목록</h4>
@@ -95,12 +95,12 @@ export const teamManageTemplate = `
     <section>
       <h3>프론트엔드 숫자야구게임 조회</h3>
       <p>팀이 매칭되었습니다.</p>
-      <ul>
+      <ul id="team-match-result">
         <li>준,포코</li>
       </ul>
       <p>
         팀을 재매칭 하시겠습니까?
-        <button>재매칭</button>
+        <button id="rematch-team-button">재매칭</button>
       </p>
     </section>
   </main>
