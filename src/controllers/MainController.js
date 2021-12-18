923734160c69e4b3f5dcb5946c72ffeaef18a7e4
최광희 <polyckh@gmail.com>
2021-12-18 13:41:22 +0900

    refactor: $ 함수를 사용하여 element 선택

--- src/controllers/MainController.js
+++ src/controllers/MainController.js
@@ -1,12 +1,12 @@
+import { $ } from '../utils/dom.js';
+
 import LayoutView from '../views/LayoutView.js';
 import TabView from '../views/TabView.js';
 
 export default {
   init() {
-    LayoutView.setup(document.querySelector('#app'));
-    TabView.setup(document.querySelector('#tab-view')).on('@changeTab', (e) =>
-      this.onChangeTabView(e.detail.tab),
-    );
+    LayoutView.setup($('#app'));
+    TabView.setup($('#tab-view')).on('@changeTab', (e) => this.onChangeTabView(e.detail.tab));
 
     this.selectedTab = '크루 관리';
   },
