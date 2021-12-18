6e9d18697279b52d5d95a65f26a96e5d4ec4c9e3
kevin7498 <kevin7498@naver.com>
2021-12-18 14:09:40 +0900

    feat: 입력받은 크루 이름이 5자를 넘어가는지 체크하는 함수 'checkLength' 구현

--- src/CrewCheck.js
+++ src/CrewCheck.js
@@ -6,4 +6,8 @@ export default class CrewCheck {
     checkEmpty() {
         return this.name !== '' && this.name.indexOf(' ') === -1; // 상수로 분리
     }
+
+    checkLength() {
+        return this.name.length <= 5; // 상수로 분리
+    }
 }
