57637611859b9b57e3236d2ba05f968a65ec93b0
ziho <ziho@kakao.com>
2021-12-18 13:58:19 +0900

    feat: 라디오 인풋 구현

--- src/components/containers/CrewManageContainer.js
+++ src/components/containers/CrewManageContainer.js
@@ -4,7 +4,10 @@ import Component from '../core/Component.js';
 
 export default class CrewManageContainer extends Component {
   init() {
-    this.$state = { crews: getLocalStorage(STORAGE_KEY.CREW, []) };
+    this.$state = {
+      crews: getLocalStorage(STORAGE_KEY.CREW, {}),
+      selected: ''
+    };
   }
 
   template() {
@@ -15,5 +18,42 @@ export default class CrewManageContainer extends Component {
     `;
   }
 
-  mounted() {}
+  mounted() {
+    this.$target.querySelector('#select-course').innerHTML = this.printSelectCourseSection();
+  }
+
+  setEvent() {
+    this.courseRadioClickHandler();
+  }
+
+  printSelectCourseSection() {
+    return `
+    <h3>크루를 관리할 코스를 선택해주세요</h3>
+    <div>
+      <input type="radio" name="course" value="frontend" id="frontend-course" />
+      <label for="frontend">프론트엔드</label>
+      <input type="radio" name="course" value="backend" id="backend-course" />
+      <label for="backend">백엔드</label>
+    </div>
+    `;
+  }
+
+  courseRadioClickHandler() {
+    this.addEvent('change', 'input[name="course"]', ({ target }) => {
+      this.setState({ selected: target.value });
+
+      this.$target.querySelector('#crew-manage').innerHTML = this.printCrewManageSection();
+    });
+  }
+
+  printCrewManageSection() {
+    return `
+    <h3>프론트엔드 크루 관리</h3>
+    <form>
+      <label>크루 이름</label>
+      <input type="text" />
+      <button>확인</button>
+    </form>
+    `;
+  }
 }
