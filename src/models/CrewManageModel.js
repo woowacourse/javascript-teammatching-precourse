451bb8bfe1378222c0437a4a2dd6775e4878401d
최광희 <polyckh@gmail.com>
2021-12-18 14:36:22 +0900

    feat: 5글자 초과된 크루 이름 입력 방지

--- src/models/CrewManageModel.js
+++ src/models/CrewManageModel.js
@@ -1,6 +1,10 @@
 export default {
   crews: { frontend: [], backend: [] },
   add(crew) {
+    if (crew.name.length > 5) {
+      alert('크루 이름은 최대 5글자만 가능합니다.');
+      return;
+    }
     if (this.crews[crew.course].findIndex((item) => item === crew.name) !== -1) {
       alert('이미 추가된 크루입니다.');
       return;
