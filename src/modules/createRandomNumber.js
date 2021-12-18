9fc01018e23f1bde6d928ffdfcdd7aa1051b013d
liswkjts <liswktjs@gmail.com>
2021-12-18 17:46:23 +0900

    feat :⚡️README업데이트 , 랜덤하게 숫자 생성하는 모듈 생성 불필요한 console.log제거 하고 , 숫자 랜덤하게 생성하는 모듈 만들고, README update README.md / createRandomNumber.js / handleMatchTeamEvent.js

---
+++ src/modules/createRandomNumber.js
@@ -0,0 +1,3 @@
+export default function createRandomNumber(arr) {
+  return MissionUtils.Random.shuffle(arr);
+}
--- src/modules/handleMatchTeamEvent.js
+++ src/modules/handleMatchTeamEvent.js
@@ -4,7 +4,6 @@ export default function handleMatchTeamEvent() {
   $('#match-team-button').addEventListener('click', () => {
     const teamNum = $('#team-member-count-input').value;
     if (checkValidCrewNumberInput(teamNum)) {
-      console.log(teamNum);
     }
   });
 }
