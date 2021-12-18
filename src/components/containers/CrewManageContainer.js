14babaced51acda0fdbdeb1a5fc1e06cefdd87cc
ziho <ziho@kakao.com>
2021-12-18 13:43:17 +0900

    feat: 크루 관리 컨테이너 첫 렌더링 시 localStorage 불러오기

--- src/components/containers/CrewManageContainer.js
+++ src/components/containers/CrewManageContainer.js
@@ -1,13 +1,19 @@
+import { STORAGE_KEY } from '../../utils/constants.js';
+import { getLocalStorage } from '../../utils/LocalStorage.js';
 import Component from '../core/Component.js';
 
 export default class CrewManageContainer extends Component {
-  init() {}
-
-  mounted() {}
+  init() {
+    this.$state = { crews: getLocalStorage(STORAGE_KEY.CREW, []) };
+  }
 
   template() {
     return `
-      crew
+      <section id="select-course"></section>
+      <section id="crew-manage"></section>
+      <section id="crew-list"></section>
     `;
   }
+
+  mounted() {}
 }
