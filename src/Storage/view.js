99b6936efc73e6ef01539e0d49ec67ceafa786b7
hozzijeong <ghwls0905@naver.com>
2021-12-18 15:16:45 +0900

    Feat: 팀 매칭 관리 View 생성

--- src/Storage/view.js
+++ src/Storage/view.js
@@ -68,3 +68,66 @@ export const CrewSecnodView = (section) => {
     
     `;
 };
+
+export const TeamFirstView = () => {
+    return `
+    <main id = 'main-container'>
+    <section>
+    <h3>팀 매칭을 관리할 코스, 미션을 선택하세요.</h3>
+    <form>
+        <select id = ${TEAM_TAB.COURSE_SELECT} >
+        <option>프론트엔드</option>
+        <option>백엔드</option>
+        </select>
+        <select> id = ${TEAM_TAB.MISSION_SELECT}
+        <option>숫자야구게임</option>
+        </select>
+        <button id = ${TEAM_TAB.SHOW_TEAM_BTN}>확인</button>
+    </form>
+    </section>
+    </main>
+    
+    
+    `;
+};
+
+export const TeamBeforeMatchingView = (team, mission) => {
+    return `
+    <section>
+    <h3>${team} ${mission} 미션의 팀 매칭</h3>
+    <div>
+        <div>
+        <p>아직 매칭된 팀이 없습니다. 팀을 매칭하겠습니까?</p>
+        <form>
+            <label>1팀당 인원 수</label>
+            <input type="number" id=${TEAM_TAB.TEAM_INPUT} />
+            <button id=${TEAM_TAB.MATCH_BTN}>팀 매칭</button>
+        </form>
+        </div>
+        <h4>크루 목록</h4>
+        <ul>
+        <li>준</li>
+        <li>포코</li>
+        </ul>
+    </div>
+    </section>
+    `;
+};
+
+export const TeamAfterMatchingView = (team, mission) => {
+    return `
+    <section>
+    <h3>${team} ${mission} 조회</h3>
+    <p>팀이 매칭되었습니다.</p>
+    <ul id=${TEAM_TAB.RESULT}>
+        <li>준,포코</li>
+    </ul>
+    <p>
+        팀을 재매칭 하시겠습니까?
+        <button id=${TEAM_TAB.REMATCH_BTN}>재매칭</button>
+    </p>
+    </section>
+    
+    
+    `;
+};
