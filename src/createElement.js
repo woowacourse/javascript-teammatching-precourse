0dae127f2b09ae14a3bf75f15d74ec392f4e92ea
NaRae-tech <narae.dev@gmail.com>
2021-12-18 15:29:25 +0900

    thead element 생성 추가

--- src/createElement.js
+++ src/createElement.js
@@ -10,6 +10,7 @@ export function pElementCreate(text) {
 }
 export function buttonIdElementCreate(condition) {
   const button = document.createElement('button');
+  button.setAttribute('type','button');
   button.id = condition.id;
   button.innerText = condition.text;
   return button;
@@ -26,8 +27,26 @@ export function inputElementCreate(condition) {
   input.type = condition.type;
   return input;
 }
+export function labelElemntCreate(condition) {
+  const label = document.createElement('label');
+  label.setAttribute("for", condition.for);
+  label.innerText = condition.text;
+  return label;
+}
 export function selectElementCreate(condition) {
   const select = document.createElement('select');
   select.id = condition.id;
   return select;
+}
+export function theadElementCreate(text) {
+  const thead = document.createElement('thead');
+  const tr = document.createElement('tr');
+  const th = document.createElement('th');
+  for (let i = 0; i < text.length; i++){
+    const tempTh = th.cloneNode(true);
+    tempTh.innerText = text[i];
+    tr.appendChild(tempTh);
+  }
+  thead.appendChild(tr);
+  return thead;
 }
