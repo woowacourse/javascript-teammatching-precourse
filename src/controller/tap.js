18ee215d07092ff59a0e5bc7b89380a145d24687
sanaandmomo <qajjang46@naver.com>
2021-12-18 14:18:47 +0900

    refactor: 탭 이벤트 리스너 반환하는 함수로 중복 제거해라

--- src/controller/tap.js
+++ src/controller/tap.js
@@ -1,15 +1,14 @@
 import { $ } from '../util/index.js';
 
-const triggerEvent = () => {
-    $('#crew-tab').addEventListener('click', () => {
-        $('#main').classList.remove('team');
-        $('#main').classList.add('crew');
-    });
+const convertTabEvent = (tab) => () => {
+    $('#main').classList.remove('crew');
+    $('#main').classList.remove('team');
+    $('#main').classList.add(tab);
+};
 
-    $('#team-tab').addEventListener('click', () => {
-        $('#main').classList.remove('crew');
-        $('#main').classList.add('team');
-    });
+const triggerEvent = () => {
+    $('#crew-tab').addEventListener('click', convertTabEvent('crew'));
+    $('#team-tab').addEventListener('click', convertTabEvent('team'));
 };
 
 export default triggerEvent;
