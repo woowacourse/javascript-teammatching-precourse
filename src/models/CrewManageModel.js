c090482a3129f38803621cd79ada0016f142615f
최광희 <polyckh@gmail.com>
2021-12-18 14:35:02 +0900

    feat: 동일한 이름의 크루가 있다면 alert하고, 다시 입력 받는다.

--- src/models/CrewManageModel.js
+++ src/models/CrewManageModel.js
@@ -1,6 +1,10 @@
 export default {
   crews: { frontend: [], backend: [] },
   add(crew) {
+    if (this.crews[crew.course].findIndex((item) => item === crew.name) !== -1) {
+      alert('이미 추가된 크루입니다.');
+      return;
+    }
     this.crews[crew.course].push(crew.name);
   },
   list() {
