d0417f29457e9dadbe11e8236c4cd3d22be6b9ef
hwangstar156 <85891751+hwangstar156@users.noreply.github.com>
2021-12-18 17:41:50 +0900

    refactor: template 상수화 처리못했던것 상수화로 추가

--- src/TeamMatching.js
+++ src/TeamMatching.js
@@ -1,5 +1,6 @@
 import CrewManageController from "./crewManage/crewManageController.js";
 import matchingManageController from "./matchingMange/matchingManageController.js";
+import { INIT_TEMPLATE } from "./template/init-template.js";
 
 export default class TeamMatching {
   constructor() {
@@ -14,22 +15,7 @@ export default class TeamMatching {
   };
 
   initPage = () => {
-    const template = `
-      <header>
-      <h1>우테코 크루와 팀 매칭 관리 보드</h1>
-      <nav>
-        <ul>
-          <li>
-            <button id="crew-tab">크루 관리</button>
-          </li>
-          <li>
-            <button id="team-tab">팀 매칭 관리</button>
-          </li>
-        </ul>
-      </nav>
-    </header>
-    `;
-    this.$container.insertAdjacentHTML("beforeend", template);
+    this.$container.insertAdjacentHTML("beforeend", INIT_TEMPLATE);
   };
 
   initComponents = () => {
---
+++ src/template/init-template.js
@@ -0,0 +1,15 @@
+export const INIT_TEMPLATE = `
+  <header>
+  <h1>우테코 크루와 팀 매칭 관리 보드</h1>
+  <nav>
+    <ul>
+      <li>
+        <button id="crew-tab">크루 관리</button>
+      </li>
+      <li>
+        <button id="team-tab">팀 매칭 관리</button>
+      </li>
+    </ul>
+  </nav>
+  </header>
+`;
