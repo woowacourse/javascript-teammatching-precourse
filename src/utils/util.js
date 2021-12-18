5a132db0ea8d23225ef31167c8f7c44b395d4603
ziho <ziho@kakao.com>
2021-12-18 17:05:19 +0900

    refactor: 랜덤 함수 함수 분리

--- src/utils/util.js
+++ src/utils/util.js
@@ -1,13 +1,8 @@
-export const matchRandomTeam = (crews, headCount) => {
-  const indexList = [];
-  crews.forEach((_, index) => {
-    indexList.push(index);
-  });
-  // eslint-disable-next-line no-undef
-  const shuffledCrewIndexList = MissionUtils.Random.shuffle(indexList);
+const matchSamePeopleTeam = (crews, headCount, shuffledCrewIndexList) => {
   const teamList = [];
   let count = 0;
   let team = [];
+
   while (shuffledCrewIndexList.length > 0) {
     team.push(crews[shuffledCrewIndexList.shift()]);
     count = count + 1;
@@ -17,11 +12,26 @@ export const matchRandomTeam = (crews, headCount) => {
       team = [];
     }
   }
+  return [teamList, team];
+};
 
-  if (team.length > 0) {
+const matchRemainder = (teamList, remainder) => {
+  if (remainder.length > 0) {
     teamList.forEach((list) => {
-      if (team.length > 0) list.push(team.shift());
+      if (remainder.length > 0) list.push(remainder.shift());
     });
   }
+};
+export const matchRandomTeam = (crews, headCount) => {
+  headCount = Number.parseInt(headCount, 10);
+  const indexList = [];
+  crews.forEach((_, index) => {
+    indexList.push(index);
+  });
+  // eslint-disable-next-line no-undef
+  const shuffledCrewIndexList = MissionUtils.Random.shuffle(indexList);
+  const [teamList, remainder] = matchSamePeopleTeam(crews, headCount, shuffledCrewIndexList);
+
+  matchRemainder(teamList, remainder);
   return teamList;
 };
