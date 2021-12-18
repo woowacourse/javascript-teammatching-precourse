4f4e0091adccf43d80b0f7f0a185786350d29492
SoYi Jeon <soyijeon47@gmail.com>
2021-12-18 17:06:14 +0900

    fix: 크루 관리 - 예외 처리 오류 수정

--- src/utils/validations.js
+++ src/utils/validations.js
@@ -1,13 +1,15 @@
 const MIN_CREW_NAME = 1;
 const MAX_CREW_NAME = 5;
 
+const isThereSpaceBeforeOrAfter = (value) => value.length !== value.trim().length;
+
 const isValidCrewNameLength = (name) => {
   if (MIN_CREW_NAME > name.length) return alert(`크루 이름은 ${MIN_CREW_NAME}자 이상이어야합니다.`);
   if (MAX_CREW_NAME < name.length) return alert(`크루 이름은 ${MAX_CREW_NAME}자 이하여야합니다.`);
   return true;
 };
 
-const isDuplicatedName = (nameToFind, crew) => {
+const isNotDuplicatedName = (nameToFind, crew) => {
   const result = crew.findIndex((crewName) => crewName === nameToFind);
   if (result !== -1) return alert('이미 존재하는 이름입니다.');
   return true;
@@ -25,12 +27,12 @@ const isSmallerThanTotalCrewNumber = (count, crew) => {
 
 function isValidCrewName(name, crew) {
   return isValidCrewNameLength(name)
-    && isDuplicatedName(name, crew);
+    && isNotDuplicatedName(name, crew);
 }
 
 function isValidTeamMemberCount(count, crew) {
   return isPositiveInteger(count)
-    && isSmallerThanTotalCrewNumber(count, crew);
+    && !isSmallerThanTotalCrewNumber(count, crew);
 }
 
 export {
