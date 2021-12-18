3ba721fbac9510d54a3638f3a44a0f03746121f2
kevin7498 <kevin7498@naver.com>
2021-12-18 13:39:07 +0900

    feat: App.js에 이벤트 리스너 추가

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import showMain from "./ShowMain.js";
-
+import MenuButtonController from "./MenuButtonController.js";
 export default class App {
     constructor() {
         showMain();
+        MenuButtonController.menuButtonEvent();
 
     }
 }
--- src/MainMenuCheck.js
+++ src/MainMenuCheck.js
@@ -1,13 +1,13 @@
 export default class MainMenuCheck {
     static checkFirstMenu(targetId) {
         if(targetId === 'crew-tab') {
-            ;
+            console.log("크루");
         }
     }
 
     static checkSecondMenu(targetId) {
         if(targetId === 'team-tab') {
-            ;
+            console.log("팀");
         }
     }
 }
