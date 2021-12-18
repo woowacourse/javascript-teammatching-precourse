ef6ae8006803d3c25ea16dea2d3fa23da8d8a5c4
kevin7498 <kevin7498@naver.com>
2021-12-18 13:45:06 +0900

    feat: html 각 요소들에 해당하는 id 삽입

--- src/utils/html.js
+++ src/utils/html.js
@@ -16,26 +16,13 @@ export const HTML_OF_HEADER = `
 
 export const HTML_OF_CREW_TAB = `
 <div id="app">
-  <header>
-    <h1>우테코 크루와 팀 매칭 관리 보드</h1>
-    <nav>
-      <ul>
-        <li>
-          <button id="crew-tab">크루 관리</button>
-        </li>
-        <li>
-          <button id="team-tab">팀 매칭 관리</button>
-        </li>
-      </ul>
-    </nav>
-  </header>
   <main>
     <section>
       <h3>크루를 관리할 코스를 선택해주세요</h3>
       <div>
-        <input type="radio" name="course" value="frontend" />
+        <input type="radio" name="course" value="frontend" id="frontend-course" />
         <label for="frontend">프론트엔드</label>
-        <input type="radio" name="course" value="backend" />
+        <input type="radio" name="course" value="backend" id="backend-course"/>
         <label for="backend">백엔드</label>
       </div>
     </section>
@@ -43,13 +30,13 @@ export const HTML_OF_CREW_TAB = `
       <h3>프론트엔드 크루 관리</h3>
       <form>
         <label>크루 이름</label>
-        <input type="text" />
-        <button>확인</button>
+        <input type="text" id="crew-name-input"/>
+        <button id="add-crew-buttton">확인</button>
       </form>
     </section>
     <section>
       <h3>프론트엔드 크루 목록</h3>
-      <table border="1">
+      <table border="1" id="crew-table">
         <thead>
           <tr>
             <th></th>
@@ -62,7 +49,7 @@ export const HTML_OF_CREW_TAB = `
             <td>1</td>
             <td>준</td>
             <td>
-              <button>삭제</button>
+              <button id="delete-crew-buttton">삭제</button>
             </td>
           </tr>
         </tbody>
@@ -73,31 +60,18 @@ export const HTML_OF_CREW_TAB = `
 
 export const HTML_OF_TEAM_TAB = `
 <div id="app">
-  <header>
-    <h1>우테코 크루와 팀 매칭 관리 보드</h1>
-    <nav>
-      <ul>
-        <li>
-          <button>크루 관리</button>
-        </li>
-        <li>
-          <button>팀 매칭 관리</button>
-        </li>
-      </ul>
-    </nav>
-  </header>
   <main>
     <section>
       <h3>팀 매칭을 관리할 코스, 미션을 선택하세요.</h3>
       <form>
-        <select>
+        <select id="course-select">
           <option>프론트엔드</option>
           <option>백엔드</option>
         </select>
-        <select>
+        <select id="mission-select">
           <option>숫자야구게임</option>
         </select>
-        <button>확인</button>
+        <button id="show-team-matcher-button">확인</button>
       </form>
     </section>
     <section>
@@ -107,12 +81,12 @@ export const HTML_OF_TEAM_TAB = `
           <p>아직 매칭된 팀이 없습니다. 팀을 매칭하겠습니까?</p>
           <form>
             <label>1팀당 인원 수</label>
-            <input type="number" />
-            <button>팀 매칭</button>
+            <input type="number" id="team-member-count-input"/>
+            <button id="match-team-button">팀 매칭</button>
           </form>
         </div>
         <h4>크루 목록</h4>
-        <ul>
+        <ul id="team-match-result">
           <li>준</li>
           <li>포코</li>
         </ul>
@@ -127,7 +101,7 @@ export const HTML_OF_TEAM_TAB = `
       </ul>
       <p>
         팀을 재매칭 하시겠습니까?
-        <button>재매칭</button>
+        <button id="rematch-team-button">재매칭</button>
       </p>
     </section>
   </main>
