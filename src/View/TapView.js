bb22d27435db756319b2447b8eab69c8c52858aa
jiyong LEE <jyman1995@gmail.com>
2021-12-18 13:30:08 +0900

    feat: 공통 탭 View 클래스 추가

---
+++ src/View/TapView.js
@@ -0,0 +1,29 @@
+import { TAP_BUTTON_ID } from '../constants';
+
+export default class TapView {
+  constructor() {
+    this.app = document.getElementById('app');
+  }
+
+  render() {
+    this.template();
+  }
+
+  template() {
+    this.app.innerHTML = `
+    <header>
+    <h1>우테코 크루와 팀 매칭 관리 보드</h1>
+    <nav>
+      <ul>
+        <li>
+          <button id=${TAP_BUTTON_ID.crew}>크루 관리</button>
+        </li>
+        <li>
+          <button id=${TAP_BUTTON_ID.team}>팀 매칭 관리</button>
+        </li>
+      </ul>
+    </nav>
+  </header>
+    `;
+  }
+}
---
+++ src/constants.js
@@ -0,0 +1,4 @@
+export const TAP_BUTTON_ID = {
+  crew: 'crew-tab',
+  team: 'team-tab',
+};
