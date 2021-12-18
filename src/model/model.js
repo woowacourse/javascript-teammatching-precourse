3e4791a6ae89a773e6f6fcf2b0d26ed8478fc928
JUDONGHYEOK <65863017+JUDONGHYEOK@users.noreply.github.com>
2021-12-18 16:16:56 +0900

    feat: localStorage를 사용하여 멤버 기억

--- src/model/model.js
+++ src/model/model.js
@@ -4,10 +4,21 @@ import Member from './member.js';
 export default class Model {
   constructor() {
     this.members = [];
+    this.init();
+  }
+
+  init() {
+    const existedMembers = JSON.parse(localStorage.getItem('members'));
+    if (existedMembers) {
+      existedMembers.forEach((member) => {
+        this.members.push(new Member(member.type, member.name));
+      });
+    }
   }
 
   addMember(type, name) {
     this.members.push(new Member(type, name));
+    localStorage.setItem('members', JSON.stringify(this.members));
   }
 
   getCrew(type) {
