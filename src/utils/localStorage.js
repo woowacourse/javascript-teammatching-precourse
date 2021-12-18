898507d28f29cd774a70082439fa1952e838cc0a
DESKTOP-L4HHRJ0\user <cks3066@naver.com>
2021-12-18 14:02:40 +0900

    feat: 추후 사용할 localStorage 유틸화

---
+++ src/utils/localStorage.js
@@ -0,0 +1,8 @@
+export function getLocalStorageItem(key) {
+    return JSON.parse(localStorage.getItem(key));
+  }
+  
+  export function setLocalStorageItem(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+  
