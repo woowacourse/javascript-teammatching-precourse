b65242417b6c80cafda91c2eb6f6675f12c44534
hozzijeong <ghwls0905@naver.com>
2021-12-18 13:30:39 +0900

    Feat: 각 기능별 상수값 추가

---
+++ src/Storage/constant.js
@@ -0,0 +1,23 @@
+export const HEADER_ID = Object.freeze({
+    CREW_TAB: "crew-tab",
+    TEAM_TAB: "team-tab",
+});
+
+export const CREW_TAB = Object.freeze({
+    FRONT_COURSE: "frontend-course",
+    BACK_COURSE: "backend-course",
+    CREW_INPUT: "crew-name-input",
+    CREW_ADD_BTN: "add-crew-button",
+    CREW_DEL_BTN: "delete-crew-button",
+    CREW_TABLE: "crew-table",
+});
+
+export const TEAM_TAB = Object.freeze({
+    COURSE_SELECT: "course-select",
+    MISSION_SELECT: "mission-select",
+    SHOW_TEAM_BTN: "show-team-matcher-button",
+    TEAM_INPUT: "team-member-count-input",
+    MATCH_BTN: "match-team-button",
+    RESULT: "team-match-result",
+    REMATCH_BTN: "rematch-team-button",
+});
