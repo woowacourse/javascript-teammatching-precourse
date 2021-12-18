ec75b7d4557beae35ca4712b7d1773c9d4e4ff0f
정현수 <jung660317@naver.com>
2021-12-18 13:27:12 +0900

    chore: Selector 상수 생성
    - 요구사항의 selector를 상수화

---
+++ src/constants.js
@@ -0,0 +1,25 @@
+export const SELECTOR = Object.freeze({
+  // 탭
+  crewTabButtonId: 'crew-tab',
+  teamTabButtonId: 'team-tab',
+
+  // 크루 관리 탭
+  frontendCourseRadioInputId: 'frontend-course',
+  backendCourseRadioInputId: 'backend-course',
+
+  crewNameInputId: 'crew-name-input',
+  addCrewButttonId: 'add-crew-buttton',
+  deleteCrewButttonId: 'delete-crew-buttton',
+
+  crewTableId: 'crew-table',
+
+  // 팀 매칭 관리 탭
+  courseSelectId: 'course-select',
+  missionSelectId: 'mission-select',
+
+  showTeamMatcherButtonId: 'show-team-matcher-button',
+  teamMemberCountInputId: 'team-member-count-input',
+  matchTeamButtonId: 'match-team-button',
+  teamMatchResultUlId: 'team-match-result',
+  rematchTeamButton: 'rematch-team-button',
+});
