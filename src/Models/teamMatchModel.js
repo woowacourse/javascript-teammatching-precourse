c60ccc2b18baf239ff2ea9387fbb61d51056e34b
kamwoo <rkadndud06@naver.com>
2021-12-18 17:32:15 +0900

    feat: team model 작성
    - 프론트와 백엔드 미션별 크루의 정보가 들어가는 모델 작성

---
+++ src/Models/teamMatchModel.js
@@ -0,0 +1,22 @@
+export const teamMatchModel = {
+  frontend: {
+    baseball: [],
+    racingcar: [],
+    lotto: [],
+    shopping_cart: [],
+    payments: [],
+    subway: [],
+    performance: [],
+    deploy: [],
+  },
+  backend: {
+    baseball: [],
+    racingcar: [],
+    lotto: [],
+    shopping_cart: [],
+    payments: [],
+    subway: [],
+    performance: [],
+    deploy: [],
+  },
+};
