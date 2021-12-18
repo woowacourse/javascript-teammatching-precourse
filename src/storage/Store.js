e15bfaa92552df32bb784edb7170ff04c5e2ccd8
simyeen <simyeen@naver.com>
2021-12-18 14:16:35 +0900

    Feat: loStorage를 사용하기 위한 Store class 구현

---
+++ src/storage/Store.js
@@ -0,0 +1,43 @@
+import initialState from "./initialState.js";
+
+class Store {
+  constructor(initialState) {
+    if (localStorage.getItem("state") === null) {
+      localStorage.setItem("state", JSON.stringify({ ...initialState }));
+    }
+    this.state = JSON.parse(localStorage.getItem("state"));
+  }
+
+  subscribe(fn) {
+    this.currentFunction = fn;
+  }
+
+  getState() {
+    return JSON.parse(localStorage.getItem("state"));
+  }
+
+  setState(key, value) {
+    this.state = {
+      ...this.state,
+      [key]: value,
+    };
+    this.notify();
+  }
+
+  notify() {
+    localStorage.setItem("state", JSON.stringify(this.state));
+    this.currentFunction(this.state);
+  }
+}
+
+const store = new Store(initialState);
+
+export default store;
+
+export function setState(key, value) {
+  store.setState(key, value);
+}
+
+export function getState() {
+  return store.getState();
+}
---
+++ src/storage/initialState.js
@@ -0,0 +1,5 @@
+const initialState = {
+  sectionID: "",
+};
+
+export default initialState;
