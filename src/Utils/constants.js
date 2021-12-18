61fbb48a0460c0e137f3e27c4fda9e2b8c8a6261
kamwoo <rkadndud06@naver.com>
2021-12-18 13:40:12 +0900

    feat: ID constant들을 작성
    - 템플릿 태그에 들어갈 id를 상수로 정리하여 작성

---
+++ src/Utils/constants.js
@@ -0,0 +1,16 @@
+export const ID = {
+  CREW_TAB: 'crew-tab',
+  TEAM_TAB: 'team-tab',
+  FRONT_RADIO: 'frontend-course',
+  BACKEND_RADIO: 'backend-course',
+  CREW_NAME_INPUT: 'crew-name-input',
+  ADD_CREW_BUTTON: 'add-crew-buttton',
+  CREW_TABLE: 'crew-table',
+  COURSE_SELECT: 'course-select',
+  MISSION_SELECT: 'mission-select',
+  SHOW_TEAM_MATCHER_BUTTON: 'show-team-matcher-button',
+  TEAM_MEMBER_COUNT_INPUT: 'team-member-count-input',
+  MATCH_TEAM_BUTTON: 'match-team-button',
+  TEAM_MATCH_RESULT: 'team-match-result',
+  REMATCH_TEAM_BUTTON: 'rematch-team-button',
+};
