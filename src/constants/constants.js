851f3d786c9f471e9365dc2c68ce4dc3a6b84af2
hanyoonseok <hys6078@naver.com>
2021-12-18 13:25:05 +0900

    feat: 상수 작성 선택자 상수 작성

---
+++ src/constants/constants.js
@@ -0,0 +1,30 @@
+export const SELECTOR = {
+  app: 'app',
+  container: 'container',
+
+  crewManageButton: 'crew-tab',
+  teamManageButton: 'team-tab',
+
+  radioFrontendInput: 'frontend-course',
+  radioBackendInput: 'backend-course',
+  crewNameInput: 'crew-name-input',
+  crewAddButton: 'add-crew-button',
+  crewDeleteButton: 'delete-crew-button',
+  crewTable: 'crew-table',
+
+  courseSelect: 'course-select',
+  missionSelect: 'mission-select',
+  teamMatcherButton: 'show-team-matcher-button',
+  memberCountInput: 'team-member-count-input',
+  matchTeamButton: 'match-team-button',
+  matchResult: 'team-match-result',
+  rematchButton: 'rematch-team-button',
+};
+
+export const WORDS = {};
+
+export const ALERT_MESSAGE = {
+  BLANK_EXIST: '공백을 제거해주세요',
+};
+
+export const LOCALSTORAGE_KEY = {};
