de730a3545aadc06f5ee23efda4afbf79ee95b95
ziho <ziho@kakao.com>
2021-12-18 16:34:29 +0900

    feat: eslint 설정 수정

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
   },
   extends: ['eslint-config-tui', 'plugin:prettier/recommended'],
   rules: {
-    indent: [2, 2, { SwitchCase: 1, ignoreComments: false, ImportDeclaration: 1 }],
     semi: 2,
     'padding-line-between-statements': 0,
     'newline-before-return': 0,
