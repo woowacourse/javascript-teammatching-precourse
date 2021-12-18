8e9e1513827df45e13d8f9fe3e21fcee39de2e2c
hozzijeong <ghwls0905@naver.com>
2021-12-18 13:50:46 +0900

    Feat: Tab 별 Presenter 띄우기 추가

--- src/Crew/crewPresenter.js
+++ src/Crew/crewPresenter.js
@@ -1,18 +1,5 @@
 export const CrewPresenter = function () {
     return `
-        <header>
-        <h1>우테코 크루와 팀 매칭 관리 보드</h1>
-        <nav>
-        <ul>
-            <li>
-            <button>크루 관리</button>
-            </li>
-            <li>
-            <button>팀 매칭 관리</button>
-            </li>
-        </ul>
-        </nav>
-    </header>
     <main>
         <section>
         <h3>크루를 관리할 코스를 선택해주세요</h3>
--- src/Storage/initialPresenter.js
+++ src/Storage/initialPresenter.js
@@ -1,21 +1,36 @@
+import { HEADER_ID as ID } from "./constant.js";
+import { CrewPresenter } from "../Crew/CrewPresenter.js";
+import { TeamPresenter } from "../Team/teamPresenter.js";
 export const InitialPresenter = function () {
     const $app = document.getElementById("app");
 
     this.init = () => {
         $app.innerHTML += header;
+        const $crewTab = document.getElementById(ID.CREW_TAB);
+        const $teamTab = document.getElementById(ID.TEAM_TAB);
+        addListener($crewTab, CrewPresenter());
+        addListener($teamTab, TeamPresenter());
     };
+
+    const addListener = (button, presenter) => {
+        button.addEventListener("click", function (e) {
+            e.preventDefault();
+            $app.innerHTML += presenter;
+        });
+    };
+
     const header = `<header>
     <h1>우테코 크루와 팀 매칭 관리 보드</h1>
     <nav>
         <ul>
             <li>
-                <button>크루 관리</button>
+                <button id = ${ID.CREW_TAB}>크루 관리</button>
             </li>
             <li>
-                <button>팀 매칭 관리</button>
+                <button id = ${ID.TEAM_TAB}>팀 매칭 관리</button>
             </li>
         </ul>
     </nav>
-</header>`;
+    </header>`;
     this.init();
 };
