49de8e3c0fc157af7e75a78999408b7dcdfdd6a5
gytks4 <gytks4@naver.com>
2021-12-18 15:13:28 +0900

    feat: select에 코스, 미션 추가

--- src/view/TeamView.js
+++ src/view/TeamView.js
@@ -7,7 +7,18 @@ export class TeamView {
     this.addElements();
   }
 
+  setOnTeamMatcherButtonClick(fn) {
+    this.$showTeamMatcherButton.addEventListener('click', (e) => {
+      e.preventDefault();
+    });
+  }
+
   addElements() {
     this.$teamMain.innerHTML = TEAM_TEMPLATE;
+    this.$courseSelect = $('#course-select');
+    this.$missionSelect = $('#mission-select');
+    this.$showTeamMatcherButton = $('#show-team-matcher-button');
+    this.$teamMemberSection = $('#team-member-section');
+    this.$teamResultSection = $('#team-result-section');
   }
 }
