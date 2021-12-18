909d288909d34e5f9c774e462da521d045f8cfca
nan-noo <uni613@naver.com>
2021-12-18 14:04:00 +0900

    feat: change content on tab click
    Create
    - crew-manage.html
    - team-manage.html
    
    Add functions <index.js>
    - renderView
    - onCrewTabClick
    - onTeamTabClick
    - eventHandler

--- src/index.js
+++ src/index.js
@@ -1,8 +1,28 @@
 import { fetchHtmlView } from './fetch.js';
 
+function renderView(fileName) {
+    fetchHtmlView(fileName).then(view => app.innerHTML = view);
+}
 
+function onCrewTabClick() {
+    renderView('crew-manage.html');
+}
+
+function onTeamTabClick() {
+    renderView('team-manage.html');
+}
+
+function eventHandler(e) {
+    e.preventDefault();
+
+    const handlers = {
+        "crew-tab"() {onCrewTabClick();},
+        "team-tab"() {onTeamTabClick();},
+    };
+    if(Object.keys(handlers).includes(e.target.id)) handlers[e.target.id]();
+}
 
 const app = document.querySelector("#app");
 
-fetchHtmlView('tab.html').then(view => app.innerHTML = view);
-
+renderView('tab.html');
+app.addEventListener('click', eventHandler);
