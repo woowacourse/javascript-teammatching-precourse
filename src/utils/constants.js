73f13767506324b111eab81bb72403c0a13e53dc
ziho <ziho@kakao.com>
2021-12-18 16:11:17 +0900

    feat: 양수인 정수가 아닌 입력값에 대한 에러메세지 상수 추가

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,7 +2,8 @@ export const ERROR_MESSAGE = Object.freeze({
   DUPLICATED_NAME: '이미 해당 크루는 등록되어 있습니다.',
   OUT_OF_RANGE: '이름은 최대 5글자까지 작성가능합니다.',
   EMPTY: '값을 입력해주세요.',
-  NOT_FOUND: '크루 목록이 비었습니다. 먼저 크루를 추가해주세요.'
+  NOT_FOUND: '크루 목록이 비었습니다. 먼저 크루를 추가해주세요.',
+  NOT_POSITIVE_INTEGER: '양수인 정수만 입력해주세요.'
 });
 
 export const STORAGE_KEY = Object.freeze({
